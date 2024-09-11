f77fc52d747663676e5c6fa4af3d5fb741f2c3f6
PQuaresma-94 <pedromquaresma@gmail.com>
2024-09-10 18:23:27 -0700

    Fix Skills style for mobile view

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -5,14 +5,16 @@ import Navbar from "../Navbar/Navbar";
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>
-        <img
-          src="/assets/icon-512x512.png"
-          alt="Logo"
-          className={styles["logo-image"]}
-        />
+      <div className={styles.header__container}>
+        <div className={styles.logo}>
+          <img
+            src="/assets/icon-512x512.png"
+            alt="Logo"
+            className={styles["logo-image"]}
+          />
+        </div>
+        <Navbar />
       </div>
-      <Navbar />
     </header>
   );
 };
